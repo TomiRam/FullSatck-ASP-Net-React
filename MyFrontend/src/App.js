e0b156c8a6213cdf9a3135b0ace9cba85f506d04
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2024-10-01 14:18:08 +0300

    Create App.js

---
+++ MyFrontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/sample')
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>React with ASP.NET Backend</h1>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
